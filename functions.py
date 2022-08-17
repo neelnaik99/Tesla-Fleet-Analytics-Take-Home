9cff13a5bd823158193bebe8d29db526edbcbb35
neelnaik99 <39886665+neelnaik99@users.noreply.github.com>
2022-08-17 07:50:56 -0700

    removed run script from functions

--- functions.py
+++ functions.py
@@ -212,7 +212,7 @@ def identify_n_different_time_series(dir_path,n=2,fraction_of_samples=0.15):
     Packages used: 
     Uses Dynamic Time Warping to compute pairwise distance between all timeseries
 
-    dtaidistance.dtw.distance_fast: https://dtaidistance.readthedocs.io/en/latest/usage/dtw.html#dtw-distance-measure-between-two-time-series
+    dtaidistance.dtw: https://dtaidistance.readthedocs.io/en/latest/usage/dtw.html#dtw-distance-measure-between-two-time-series
 
     Time_complexity:  O(N^2*M^2)
         where N is the length of the time series
@@ -276,40 +276,5 @@ def identify_n_different_time_series(dir_path,n=2,fraction_of_samples=0.15):
     logger.info(f"The {n} most different time series are:")
 
     return list_max_dist[:n]
-
-
-
-CURRENT_PATH =  os.path.dirname(os.path.realpath(__file__))
-config_file = 'config.ini'
-
-config_file_path = os.path.join(CURRENT_PATH,config_file)
-parser = configparser.ConfigParser()
-parser.read(config_file_path)
-
-data_folder =  parser['input']['data_folder']
-csv_file_name = parser['input']['csv_name']
-
-CURRENT_PATH =  os.path.dirname(os.path.realpath(__file__))
-INPUT_DIR = os.path.join(CURRENT_PATH,data_folder)
-INPUT_FILE = os.path.join(INPUT_DIR,csv_file_name)
-
-
-
-print('least 5 common values by occurence')
-print(select_n_values_by_occurence(INPUT_FILE,n=5,least_common=True))
-
-print('The 3 most common values by total time spent at that value')
-print(select_n_values_by_time_spent(INPUT_FILE,n=3,least_common=False))
-
-print('The 3 cycles with the largest amplitude')
-print(select_n_cycles(INPUT_FILE,n=3,smallest=False))
-
-print('The 2 Vehicle which are behaving differently from others ')
-print(identify_n_different_time_series(INPUT_DIR,n=2,fraction_of_samples=0.15))
-
-
-
-
-
 
 
